dc913d02703c38a81e04b0c63b2167d9c73c895c
Norbert Kovacs <19knorbert95@gmail.com>
2020-07-22 15:28:41 +0200

    idleTimeoutMillis added in  knex config

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 module.exports = {
   client: 'pg',
   connection: process.env.DB_URL,
+  pool: {
+    idleTimeoutMillis: 300000,
+  },
   migrations: {
     directory: './data/migrations',
   },
