2cd419a4842b20c22efe6f193f1eea5e73952482
Norbert Kovacs <19knorbert95@gmail.com>
2020-07-22 16:02:09 +0200

    removed idleTimeoutMillis and npm run test

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,6 @@ require('dotenv').config();
 module.exports = {
   client: 'pg',
   connection: process.env.DB_URL,
-  pool: {
-    idleTimeoutMillis: 300000,
-  },
   migrations: {
     directory: './data/migrations',
   },
