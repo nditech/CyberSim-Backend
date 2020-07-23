4bd022bcea2b60d2494cf68f2d03bda4b52a55b7
Roland Szoke <fwalkwithm@gmail.com>
2020-07-23 15:06:49 +0200

    Create test db with docker

---
+++ docker_postgres_init.sql
@@ -0,0 +1,9 @@
+CREATE USER test WITH PASSWORD 'test' CREATEDB;
+CREATE DATABASE ndi_test
+    WITH
+    OWNER = test
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
